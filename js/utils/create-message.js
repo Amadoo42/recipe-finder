ec2a8942d32056f104f67e6ccddddb1807b02b0a
Muhammed Esmail <muhammedesmail101@gmail.com>
2026-03-18 12:18:29 +0200

    Document createMessage function
    Added documentation comments for createMessage function.

--- js/utils/create-message.js
+++ js/utils/create-message.js
@@ -1,7 +1,24 @@
-export function createMessage(success, description, data=null){
-    return{
-        success: success,
-        description: description,
+/*
+
+createMessage(success, description, data) function:
+    - Standardizes the communication between the logic layer and the UI handlers
+    - Expects the input in the form of:
+    {
+        success: Boolean,      // Whether the operation worked
+        description: String,   // A human-readable status or error message
+        data: Object/Null      // Optional payload (e.g., user object or session object)
+    }
+    - Purpose:
+        - Provide a consistent structure for all function responses
+    - return:
+        returns a structured object confirming success with the necessary payload (if applicable) 
+        or describing the specific issue
+
+*/
+export function createMessage(success, description, data = null) {
+    return {
+        success,
+        description,
         data
     };
 }
