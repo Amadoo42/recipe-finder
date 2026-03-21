c418603babff0a351456789e08dd6b1664bc259e
Muhammed-Esmail <muhammedesmail101@gmail.com>
2026-03-21 21:34:51 +0200

    addition: user data attributes

--- js/init-UI.js
+++ js/init-UI.js
@@ -5,6 +5,20 @@ export function initUI(user) {
     buildNav();
     loadGoogleFonts();
 
+    // add user data
+    if (user) {
+
+        const DATA_FIELDS = ['firstName', 'lastName', 'username', 'email', 'role'];
+
+        DATA_FIELDS.forEach(field => {
+            if (user[field]) {
+                document.querySelectorAll(`[data-${field}]`).forEach(dataField => {
+                    dataField.textContent = user[field];
+                });
+            }
+        });
+    }
+
     // Pages are hidden by default. Show the hidden body of the page.
     document.body.classList.add('ready');
 }
