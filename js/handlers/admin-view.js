8fcf6f8f7f991d5ab2b8935c2e240f2ba23bfd83
Lashin <seif.mlashin1911@gmail.com>
2026-03-24 22:45:13 +0200

    edit and delete functionality synced with DB
    This commit addresses the syncing of 3 main functions edit recipe and delete recipe and rendering recipe all from the DB

--- js/handlers/admin-view.js
+++ js/handlers/admin-view.js
@@ -1,119 +1,37 @@
-const recipes = [
-  {
-    id: 1,
-    name: "Classic Cheeseburger",
-    course: "Main",
-    description: "A juicy beef patty topped with melted cheddar cheese, lettuce, and tomato.",
-    ingredients: [
-      {
-        name: "Beef patty",
-        quantity: 1,
-        unit: "piece"
-      },
-      {
-        name: "Cheddar cheese",
-        quantity: 2,
-        unit: "slices"
-      },
-      {
-        name: "Hamburger bun",
-        quantity: 1,
-        unit: "piece"
-      }
-    ]
-  },
-  {
-    id: 2,
-    name: "Caesar Salad",
-    course: "Starter",
-    description: "Crisp romaine lettuce tossed in Caesar dressing, topped with croutons and parmesan.",
-    ingredients: [
-      {
-        name: "Romaine lettuce",
-        quantity: 2,
-        unit: "cups"
-      },
-      {
-        name: "Caesar dressing",
-        quantity: 3,
-        unit: "tablespoons"
-      },
-      {
-        name: "Parmesan cheese",
-        quantity: 0.5,
-        unit: "cup"
-      }
-    ]
-  },
-  {
-    id: 3,
-    name: "Pizza",
-    course: "Main",
-    description: "Classic Margherita pizza with a thick crust, tomato sauce, and mozzarella.",
-    ingredients: [
-      {
-        name: "Pizza dough",
-        quantity: 1,
-        unit: "piece"
-      },
-      {
-        name: "Mozzarella cheese",
-        quantity: 2,
-        unit: "cups"
-      },
-      {
-        name: "Tomato sauce",
-        quantity: 0.5,
-        unit: "cup"
-      }
-    ]
-  },
-  {
-    id: 4,
-    name: "Chocolate Lava Cake",
-    course: "Dessert",
-    description: "Warm chocolate cake with a gooey, molten chocolate center.",
-    ingredients: [
-      {
-        name: "Dark chocolate",
-        quantity: 100,
-        unit: "grams"
-      },
-      {
-        name: "Butter",
-        quantity: 0.5,
-        unit: "cup"
-      },
-      {
-        name: "Sugar",
-        quantity: 3,
-        unit: "tablespoons"
-      }
-    ]
-  }
-];
-//note id is int
+import { getRecipes, deleteRecipe } from "../database/db-recipes.js";
 
-//this function is used to initilize the recipe array
-//function needs is getRecipes which returns an array containing recipe objects
+let recipes = [];
+
+/**
+ * @brief this function initilizes the recipes array from database
+ */
 function init() {
-    //getRecipes();
+    recipes = getRecipes();
 }
 
+/**
+ * @brief this function redirects the user to the edit page
+ * @param {int} id 
+ */
 function editRecipe(id){
     window.location.href = `add-recipe.html?RecipeID=${id}&Edit=1`;
 }
 
-//this function is used to delete a recipe from UI and DB
-//this function needs a deleteRecipe(id) function that delete recipe of "id" in the database
-function deleteRecipe(id){
+/**
+ * @brief this function deletes a recipe of certain id
+ * @param {int} id 
+ */
+function deleteRecipeView(id){
     if(confirm("Do you want to marry me?<3")){
-        const article = document.getElementById(id);
+        const article = document.getElementById(`A${id}`);
         article.remove();
-        //deleteRecipe(id);
+        deleteRecipe(id);
     }
 }
 
+/**
+ * @brief this handles the rendering of each recipe card
+ */
 function renderRecipes() {
     const container = document.getElementById('main');
     container.innerHTML = "";
@@ -127,10 +45,18 @@ function renderRecipes() {
             <p>${recipe.name}</p>
             <p>${recipe.description}</p>
             <div>
-                <button onclick = "editRecipe(${recipe.id})">Edit</button>
-                <button onclick = "deleteRecipe('${id}')">Delete</button>
+                <button class="EditBtn">Edit</button>
+                <button class="DeleteBtn">Delete</button>
             </div>
         `;
+
+        article.querySelector('.EditBtn').addEventListener('click', () => {
+          editRecipe(recipe.id);
+        })
+        article.querySelector('.DeleteBtn').addEventListener('click', () => {
+          deleteRecipeView(recipe.id);
+        })
+
         container.appendChild(article);
     });
 }
--- js/handlers/edit-recipe.js
+++ js/handlers/edit-recipe.js
@@ -1,158 +1,67 @@
-const recipes = [
-    {
-        id: 1,
-        name: 'Classic Cheeseburger',
-        course: 'Main',
-        description:
-            'A juicy beef patty topped with melted cheddar cheese, lettuce, and tomato.',
-        ingredients: [
-            {
-                name: 'Beef patty',
-                quantity: 1,
-                unit: 'piece',
-            },
-            {
-                name: 'Cheddar cheese',
-                quantity: 2,
-                unit: 'slices',
-            },
-            {
-                name: 'Hamburger bun',
-                quantity: 1,
-                unit: 'piece',
-            },
-        ],
-    },
-    {
-        id: 2,
-        name: 'Caesar Salad',
-        course: 'Appetizers',
-        description:
-            'Crisp romaine lettuce tossed in Caesar dressing, topped with croutons and parmesan.',
-        ingredients: [
-            {
-                name: 'Romaine lettuce',
-                quantity: 2,
-                unit: 'cups',
-            },
-            {
-                name: 'Caesar dressing',
-                quantity: 3,
-                unit: 'tablespoons',
-            },
-            {
-                name: 'Parmesan cheese',
-                quantity: 0.5,
-                unit: 'cup',
-            },
-        ],
-    },
-    {
-        id: 3,
-        name: 'Pizza',
-        course: 'Main',
-        description:
-            'Classic Margherita pizza with a thick crust, tomato sauce, and mozzarella.',
-        ingredients: [
-            {
-                name: 'Pizza dough',
-                quantity: 1,
-                unit: 'piece',
-            },
-            {
-                name: 'Mozzarella cheese',
-                quantity: 2,
-                unit: 'cups',
-            },
-            {
-                name: 'Tomato sauce',
-                quantity: 0.5,
-                unit: 'cup',
-            },
-        ],
-    },
-    {
-        id: 4,
-        name: 'Chocolate Lava Cake',
-        course: 'Dessert',
-        description:
-            'Warm chocolate cake with a gooey, molten chocolate center.',
-        ingredients: [
-            {
-                name: 'Dark chocolate',
-                quantity: 100,
-                unit: 'grams',
-            },
-            {
-                name: 'Butter',
-                quantity: 0.5,
-                unit: 'cup',
-            },
-            {
-                name: 'Sugar',
-                quantity: 3,
-                unit: 'tablespoons',
-            },
-        ],
-    },
-];
+import { getRecipeById, getRecipes } from "../database/db-recipes.js";
 
+/*
+This block of code reads the url and sees if it is edit page
+and initializes the recipe
+*/
 const queryString = window.location.search;
-
 const params = new URLSearchParams(queryString);
-
-const recipeID = params.get('RecipeID') - 1;
+const recipeID = params.get('RecipeID');
 const isEdit = params.get('Edit');
 
-//this function is used to initilize the recipe array
-//function needs is getRecipes which returns an array containing recipe objects
-function init() {
-    //getRecipes();
-}
+let recipe = getRecipeById(recipeID);
 
+/**
+ * @brief this only edits the html to make it edit page
+ */
 function editHtml() {
     const header = document.getElementById('recipe');
     header.innerText = 'Edit Recipe';
 
-    const button = document.getElementById('add-recipe');
+    const button = document.getElementById('addRecipe');
     button.innerText = 'Save';
 }
 
+/**
+ * @brief this loads the form with recipe details
+ */
 function loadRecipe() {
-    const inputName = document.getElementById('recipe-name');
-    inputName.value = `${recipes[recipeID].name}`;
+    const inputName = document.getElementById('recipeName');
+    inputName.value = `${recipe.name}`;
 
     const selectName = document.getElementById('course');
-    selectName.value = `${recipes[recipeID].course}`;
+    selectName.value = `${recipe.courseType}`;
 
     const description = document.getElementById('description');
-    description.value = recipes[recipeID].description;
+    description.value = recipe.description;
 
-    const ingredients = document.getElementById('ingredient-list');
+    const ingredients = document.getElementById('ingredientList');
     ingredients.innerHTML = '';
 
-    recipes[recipeID].ingredients.forEach((ingredient) => {
+    recipe.ingredients.forEach((ingredient) => {
         const item = document.createElement('li');
         const name = ingredient.name;
         const quantity = ingredient.quantity;
         const unit = ingredient.unit;
 
         item.innerHTML = `
-        <div class="item-container">
-            <p class="ingredient-name" style="color: black;">${name}</p>
+            <div class="ItemContainer">
+            <p class="IngredientName">${name}</p>
             <div>
-                <p class="ingredient-quantity">${quantity}</p>
-                <p class="ingredient-unit">${unit}</p>
+                <p class="IngredientQuantity">${quantity}</p>
+                <p class="IngredientUnit">${unit}</p>
             </div>
         </div>
-        <button class="delete-btn">X</button>
+        <button class="DeleteBtn">X</button>
         `;
-        item.querySelector('.delete-btn').addEventListener(
+
+        item.querySelector('.DeleteBtn').addEventListener(
             'click',
             function () {
                 item.remove();
             },
         );
+
         ingredients.appendChild(item);
     });
 }
